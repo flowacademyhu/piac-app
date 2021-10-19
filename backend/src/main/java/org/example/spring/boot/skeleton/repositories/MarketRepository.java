9b597aedeb24406f1c7aaf8fa65de5f39bda15c0
Kővári Kristóf <kovarik8505@gmail.com>
2021-10-19 16:37:09 +0200

    Market netity, service added; dependencies, docker-compose set

---
+++ backend/src/main/java/org/example/spring/boot/skeleton/repositories/MarketRepository.java
@@ -0,0 +1,9 @@
+package org.example.spring.boot.skeleton.repositories;
+
+import org.example.spring.boot.skeleton.entities.Market;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarketRepository extends JpaRepository<Market, Long> {
+}
