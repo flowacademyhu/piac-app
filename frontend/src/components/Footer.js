dd42f74cc95a5b02c61ec92373f93c30f3dc773b
Erik <patoerik93@gmail.com>
2021-10-20 16:22:20 +0200

    Added Header and footer for markets

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,42 @@
+import './Header.css';
+import { Link } from 'react-router-dom';
+import { Container, Row,Col } from 'react-bootstrap';
+import VendorLogo from '../icons/navigation/vendor_dark.svg'
+import VendorLogoHighlight from '../icons/navigation/vendor_primary.svg'
+import MarketLogo from '../icons/navigation/market_dark.svg'
+import MarketLogoHighlight from '../icons/navigation/market_primary.svg'
+
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+
+const Footer = () => {
+
+    const location = useLocation();
+
+  return (
+    <div className="footer">
+    <Container className='container-fluid'>
+        <Row>
+            <Col style={{cursor:'pointer'}}>
+                <Link className="footer-link" to="/market">
+                <img src={location.pathname == "/market" ? MarketLogoHighlight : MarketLogo} alt='Logo' />
+                    <div id='market-label'>
+                        PIACOK
+                    </div>
+                </Link>
+            </Col>
+            <Col style={{cursor:'pointer'}}>
+                <Link className="footer-link" to='/vendor'>
+                <img src={location.pathname == "/vendor" ? VendorLogoHighlight : VendorLogo} alt='Logo' />
+                    <div id='vendor-label'>
+                        ÁRUSOK
+                    </div>
+                </Link>
+            </Col>
+        </Row>
+    </Container>
+    </div>
+  );
+}
+
+export default Footer;
+
