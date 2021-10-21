8a938b8d70cffeb7904c9cc25a8fcc8501a5cbc2
Szabó Gábor <91szabogabor@gmail.com>
2021-10-21 10:40:57 +0200

    modify MarketCard to fit css format

--- frontend/src/components/MarketCard.js
+++ frontend/src/components/MarketCard.js
@@ -1,73 +1,7 @@
-const card = {
-  display: 'grid',
-  gridTemplateColumns: '1fr 1fr',
-  gridTemplateRows: 'auto 1fr auto',
-  cursor: 'pointer',
-  userSelect: 'none',
-  borderRadius: '10px',
-  boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 10px',
-  marginTop: '30px',
-  paddingTop: '20px',
-  paddingBottom: '20px',
-  backgroundColor: 'white',
-  width: '95%',
-  margin: 'auto',
-  minWidth: '480px'
-};
-
-const picture = {
-  height: '110px',
-  borderRadius: '80px',
-  gridColumn: '1',
-  gridRowStart: '1',
-  gridRowEnd: '4',
-  borderStyle: 'solid',
-  borderWidth: '1px',
-  borderColor: 'lightgrey',
-  alignSelf: 'center',
-  margin: 'auto'
-};
-
-const marketName = {
-  gridColumn: '2',
-  gridRow: '1',
-  fontFamily: 'Open Sans, sansSerif',
-  fontSize: '20px',
-  fontWeight: 'bold',
-  overflowWrap: 'break-word'
-};
-
-const marketLocationAndDate = {
-  gridColumn: '2',
-  gridRow: '2',
-  fontFamily: 'Open Sans, sansSerif',
-  fontSize: '12px',
-  overflowWrap: 'break-word'
-};
-
-const vendorNumber = {
-  gridColumn: '2',
-  gridRow: '3',
-  fontFamily: 'Open Sans, sansSerif',
-  fontSize: '12px',
-  overflowWrap: 'break-word'
-};
+import './MarketCard.css';
 
 const body = document.body;
 body.style.background = '#F7F5F2';
-
-const emptySpaceOnTop = {
-  marginTop: '30px'
-};
-
-const parallelDateAndHour = {
-  display: 'flex',
-  alignItems: 'baseline'
-};
-
-const startAndEndHours = {
-  fontSize: '10px'
-};
 
 const MarketCard = ({
   title,
@@ -79,20 +13,20 @@ const MarketCard = ({
 }) => {
   return (
     <>
-      <div style={emptySpaceOnTop} />
-      <div style={card}>
-        <img style={picture} src={imageLogo} alt="logo" />
-        <div div style={marketName}>
+      <div className="emptySpaceOnTop" />
+      <div className="card">
+        <img className="picture" src={imageLogo} alt="logo" />
+        <div div className="marketName">
           {title}
         </div>
-        <div div style={marketLocationAndDate}>
+        <div div className="marketLocationAndDate">
           <div>{location}</div>
-          <div style={parallelDateAndHour}>
+          <div className="parallelDateAndHour">
             <div>{date}</div>
-            <div style={startAndEndHours}>{lat}</div>
+            <div className="startAndEndHours">{lat}</div>
           </div>
         </div>
-        <div div style={vendorNumber}>
+        <div div className="vendorNumber">
           {vendorsAmount}
         </div>
       </div>
