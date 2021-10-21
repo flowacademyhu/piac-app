05eb85ddd8060a120de810c0b5cff61b6aa636c5
Erik <patoerik93@gmail.com>
2021-10-21 10:39:53 +0200

    Routing for footer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,30 @@
 import '../styles/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header.js';
+import VendorHeader from './HomeVendorHeader';
+import Footer from './Footer';
+import './Header.css';
 
 const App = () => {
   return (
     <div className="App">
-      Hello Lambda
+      <div className="container-fluid">
+        <Router>
+          <Switch>
+            <Route path="/market">
+              <Header />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/vendor">
+              <VendorHeader />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
