c21b58cfe17c90a1a8eb6f4a0219596b365e3f46
SIsák Ádám <sisakadam@gmail.com>
2021-10-21 19:51:34 +0200

    webconfig file modified correctly (probably)

--- backend/src/main/java/org/example/spring/boot/skeleton/config/WebConfig.java
+++ backend/src/main/java/org/example/spring/boot/skeleton/config/WebConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
