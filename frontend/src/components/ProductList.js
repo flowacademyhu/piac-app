541ffb88fa6d5b2cf728f2a305991909c63fed3c
Attila Csanyi <csaniattila23@gmail.com>
2021-10-21 21:35:56 +0200

    Refactor ProductList

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const ProductList = ({ products }) => {
-  const productList = products.map((product) => (
-    <div key={product} className="Product-Item">
-      {product}
-    </div>
-  ));
+  const productList = products
+    ? products.map((product) => (
+        <div key={product} className="Product-Item">
+          {product}
+        </div>
+      ))
+    : [];
   return <div className="Product-List">{productList}</div>;
 };
 export default ProductList;
