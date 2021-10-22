1916c8897653ac76597cee601612baa319ec6ae5
Kővári Kristóf <kovarik8505@gmail.com>
2021-10-22 11:45:19 +0200

    modifying DemoApplication

--- backend/src/main/java/org/example/spring/boot/skeleton/DemoApplication.java
+++ backend/src/main/java/org/example/spring/boot/skeleton/DemoApplication.java
@@ -1,23 +1,25 @@
 package org.example.spring.boot.skeleton;
 
 import org.example.spring.boot.skeleton.controller.MarketController;
-import org.example.spring.boot.skeleton.entities.Vendor;
+import org.example.spring.boot.skeleton.controller.VendorController;
 import org.example.spring.boot.skeleton.model.MarketDTO;
+import org.example.spring.boot.skeleton.model.VendorDTO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
 	private final MarketController marketController;
+	private final VendorController vendorController;
 
-	public DemoApplication(MarketController marketController) {
+	public DemoApplication(MarketController marketController, VendorController vendorController) {
 		this.marketController = marketController;
+		this.vendorController = vendorController;
 	}
 
 	public static void main(String[] args) {
@@ -39,7 +41,13 @@ public class DemoApplication implements CommandLineRunner {
 		marketDTO2.setPlace("Szeged Pláza");
 		marketController.addMarket(marketDTO2);
 
-
+//		VendorDTO vendorDTO1 = new VendorDTO();
+//		vendorDTO1.setName("Chilikirály");
+//		vendorDTO1.setIntro("Chilizz belünk, Magyarország legjobb chilijeivel...");
+//		vendorDTO1.setMarketId(2L);
+//		Set<String> newSet = Set.of("chilik","paprikakrémek","csípős szószok");
+//		vendorDTO1.setProducts(newSet);
+//		vendorController.addVendorToMarket(vendorDTO1);
 	}
 
 
