13491bd149529c1dbfaa4601a2ed167de8fce61c
Zsolt Rostás <ocsisajt@vipmail.hu>
2021-10-26 09:59:44 +0200

    added links to vendor contacts

--- frontend/src/components/VendorContacts.js
+++ frontend/src/components/VendorContacts.js
@@ -1,44 +1,49 @@
-import emailLogo from './icons/social/email.svg';
-import facebookLogo from './icons/social/facebook.svg';
-import instagramLogo from './icons/social/instagram.svg';
-import phoneLogo from './icons/social/phone.svg';
-import webLogo from './icons/social/web.svg';
-import '../styles/VendorContacts.css';
+import emailLogo from "./icons/social/email.svg";
+import facebookLogo from "./icons/social/facebook.svg";
+import instagramLogo from "./icons/social/instagram.svg";
+import phoneLogo from "./icons/social/phone.svg";
+import webLogo from "./icons/social/web.svg";
+import "../styles/VendorContacts.css";
 
 const VendorContacts = (props) => {
   const { facebook, instagram, website, email, phone } = props;
   const contacts = [
     {
       title: facebook,
-      logo: facebookLogo
+      logo: facebookLogo,
+      link: "https:",
     },
     {
       title: instagram,
-      logo: instagramLogo
+      logo: instagramLogo,
+      link: "https:",
     },
     {
       title: website,
-      logo: webLogo
+      logo: webLogo,
+      link: "https:",
     },
     {
       title: email,
-      logo: emailLogo
+      logo: emailLogo,
+      link: "mailto:",
     },
     {
       title: phone,
-      logo: phoneLogo
-    }
+      logo: phoneLogo,
+      link: "tel:",
+    },
   ];
 
   return (
-    <div className='container'>
-      <h1 className='title'>Elérhetőségek</h1>
+    <div className="container">
+      <h1 className="title">Elérhetőségek</h1>
       {contacts.map((contact) => {
         return (
           contact.title && (
-            <div className='info'>
-              <img src={contact.logo} className='logo' alt='' />
-              <span>{contact.title}</span>
+            <div className="info">
+              <img src={contact.logo} className="logo" alt="" />
+              <a href={contact.link + contact.title}>{contact.title}</a>
             </div>
           )
         );
