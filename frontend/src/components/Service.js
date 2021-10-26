8a321a9df8bb40e043e6aee8f02c0b01f8b9a13c
Erik <patoerik93@gmail.com>
2021-10-26 13:58:48 +0200

    Added seperate api for VendorCardList

--- frontend/src/components/Service.js
+++ frontend/src/components/Service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const MarketAPI = 'http://localhost:8081/v1/api/market';
+const MarketAPI = 'http://localhost:8082/v1/api/market';
 
 export const fetchMarkets = async () => {
   let url = MarketAPI;
@@ -9,7 +9,20 @@ export const fetchMarkets = async () => {
     const data = response.data;
     return data;
   } catch (error) {
-    console.log('failed to fetch Markets');
+    console.log('Failed to load Markets');
     throw Error(error);
+  }
+};
+
+const VendorAPI = 'http://localhost:8082/v1/api/vendor';
+
+export const fetchVendors = async () => {
+  let url = VendorAPI;
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.log('Failed to load vendors');
   }
 };
