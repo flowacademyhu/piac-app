9d18e62035085c0febf9c448ca32c492e14456ad
SIsák Ádám <sisakadam@gmail.com>
2021-10-26 15:43:39 +0200

    small improvement

--- frontend/src/components/VendorsByMarketPage.js
+++ frontend/src/components/VendorsByMarketPage.js
@@ -2,41 +2,57 @@ import Footer from '../components/Footer';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import VendorCardList from '../components/VendorCardList';
+import VendorCard from '../components/VendorCard';
 
 const VendorsByMarketPage = () => {
   const { id } = useParams();
+  const [hasError, setHasError] = useState(null);
 
   const [marketById, setMatketById] = useState([]);
 
-  const getMarketById = async () => {
-    const response = await axios.get(
-      `http://localhost:8081/v1/api/market/${id}`
-    );
-
-    setMatketById(response.data);
-    console.log(marketById);
-  };
-
-  useEffect(() => {
-    getMarketById();
+  useEffect(async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8081/v1/api/market/${id}`
+      );
+      setMatketById(response.data);
+    } catch (err) {
+      setHasError(true);
+    }
   }, [id]);
 
+  console.log(marketById);
   return (
     <>
       <div className="marketHeader">
-        <img className="marketLogo" src={marketById.profilePic} alt="logo" />
-        <div div className="marketName">
+        <img
+          style={{ height: '120px' }}
+          className="marketLogo"
+          src={marketById.profilePic}
+          alt="logo"
+        />
+        <div
+          div
+          className="marketName"
+          style={{ paddingLeft: '20px', fontSize: '40px' }}
+        >
           {marketById.name}
         </div>
-        <div div className="marketLocationAndDate">
+        <div
+          div
+          className="marketLocationAndDate"
+          style={{ paddingLeft: '20px', fontSize: '20px' }}
+        >
           <div>{marketById.place}</div>
           <div className="parallelDateAndHour">
             <div>{marketById.date}</div>
+            <div className="startAndEndHour">
+              {' ' + marketById.standAndEndHour}
+            </div>
           </div>
         </div>
       </div>
-      <VendorCardList />
+      <div className="card-list" />
       <Footer />
     </>
   );
