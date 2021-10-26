016b888c176ca0c7d06ae1077dfb489835e19b83
Erik <patoerik93@gmail.com>
2021-10-26 14:01:59 +0200

    Modified error

--- frontend/src/components/Service.js
+++ frontend/src/components/Service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const MarketAPI = 'http://localhost:8082/v1/api/market';
+const MarketAPI = 'http://localhost:8081/v1/api/market';
 
 export const fetchMarkets = async () => {
   let url = MarketAPI;
@@ -9,12 +9,12 @@ export const fetchMarkets = async () => {
     const data = response.data;
     return data;
   } catch (error) {
-    console.log('Failed to load Markets');
+    console.log('Failed to load markets');
     throw Error(error);
   }
 };
 
-const VendorAPI = 'http://localhost:8082/v1/api/vendor';
+const VendorAPI = 'http://localhost:8081/v1/api/vendor';
 
 export const fetchVendors = async () => {
   let url = VendorAPI;
