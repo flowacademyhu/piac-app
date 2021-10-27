989828ae7d1e01c42f11ce692a3b18abc869edc4
Kővári Kristóf <kovarik8505@gmail.com>
2021-10-27 15:35:46 +0200

    DetailVendorDTO set with id

--- backend/src/main/java/org/example/spring/boot/skeleton/model/DetailVendorDTO.java
+++ backend/src/main/java/org/example/spring/boot/skeleton/model/DetailVendorDTO.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class DetailVendorDTO {
 
+    private Long id;
     private String name;
     private String intro;
     private Set<String> products = new HashSet<>();
@@ -103,5 +104,18 @@ public class DetailVendorDTO {
     public DetailVendorDTO setWebSite(String webSite) {
         this.webSite = webSite;
         return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public DetailVendorDTO setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public boolean isCardPayment() {
+        return cardPayment;
     }
 }
