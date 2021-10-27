248aaea708e54f275be99d837ba56cf78f45919a
SIsák Ádám <sisakadam@gmail.com>
2021-10-27 09:23:10 +0200

    vendorlist upload progress files added from Zsolti

---
+++ frontend/src/components/VendorlistUploadInProgress.js
@@ -0,0 +1,21 @@
+import '../styles/VendorlistUploadInProgress.css';
+
+const VendorlistUploadInProgress = () => {
+  return (
+    <div className='uploadScreen'>
+      <div className='uploadTexts middleUploadText'>
+        <span>Szervezés alatt...</span>
+      </div>
+      <div className='uploadTexts bottomInfoText'>
+        <div>
+          <span>Itt fogod megtalálni az árusokat,</span>
+        </div>
+        <div>
+          <span>akik ezen a piacon jelen lesznek.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VendorlistUploadInProgress;
