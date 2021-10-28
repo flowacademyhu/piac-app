df7ac3c15a0aaeee82b2787f8b4a9f388f635d6f
dzsak <sisakadam@gmail.com>
2021-10-28 14:21:30 +0200

    profile picture of vendors now getting from backend

--- frontend/src/components/VendorCardList.js
+++ frontend/src/components/VendorCardList.js
@@ -4,9 +4,6 @@ import { fetchVendors } from './Service';
 import VendorCard from './VendorCard';
 import { Link } from 'react-router-dom';
 
-const imageLogo =
-  'https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/128194749_3718400788226892_2631429779387369230_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=973b4a&_nc_ohc=ZRn8DPeEpMgAX_e0Aw-&_nc_ht=scontent-vie1-1.xx&oh=b25f36537a474921e7c1bf971c277d3c&oe=6195513B';
-
 const VendorCardList = () => {
   const [vendors, setVendors] = useState([]);
 
@@ -19,6 +16,8 @@ const VendorCardList = () => {
     getVendors();
   }, []);
 
+  console.log(vendors);
+
   return (
     <div className="card-list">
       {vendors.map((post) => {
@@ -26,7 +25,7 @@ const VendorCardList = () => {
           <div key={post.id}>
             <Link to={`/arusok/${post.id}`} style={{ textDecoration: 'none' }}>
               <VendorCard
-                imageLogo={imageLogo}
+                imageLogo={post.profilePic}
                 vendor={post.name}
                 vendorDesc={post.intro}
               />
--- frontend/src/components/VendorListOfOneMarket.js
+++ frontend/src/components/VendorListOfOneMarket.js
@@ -1,22 +1,19 @@
 import { Link } from 'react-router-dom';
 import VendorCard from '../components/VendorCard';
 
-const imageLogo =
-  'https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/128194749_3718400788226892_2631429779387369230_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=973b4a&_nc_ohc=ZRn8DPeEpMgAX_e0Aw-&_nc_ht=scontent-vie1-1.xx&oh=b25f36537a474921e7c1bf971c277d3c&oe=6195513B';
-
 const VendorListOfOneMarket = ({ market }) => {
   return (
-    <div className='card-list'>
+    <div className="card-list">
       {market.vendors &&
-        market.vendors.map((vendor, i) => {
+        market.vendors.map((vendor) => {
           return (
-            <div key={i}>
+            <div key={vendor.id}>
               <Link
                 to={`/arusok/${vendor.id}`}
                 style={{ textDecoration: 'none' }}
               >
                 <VendorCard
-                  imageLogo={imageLogo}
+                  imageLogo={vendor.profilePic}
                   vendor={vendor.name}
                   vendorDesc={vendor.intro}
                 />
