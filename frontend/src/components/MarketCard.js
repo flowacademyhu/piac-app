c904e57b44c642b154c9b51adf9d5a61c9bb79ff
dzsak <sisakadam@gmail.com>
2021-10-29 12:48:37 +0200

    outsourcing market info and time converter

--- frontend/src/components/MarketCard.js
+++ frontend/src/components/MarketCard.js
@@ -12,45 +12,16 @@ const MarketCard = ({
   vendorsAmount,
   profilePic
 }) => {
-  const marketOpeningDateFormatter = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  };
-
-  const marketMinuteFormatter = {
-    hour: '2-digit',
-    minute: 'numeric'
-  };
-
-  const formattedYearMonthAndDay = new Intl.DateTimeFormat(
-    'hu-HU',
-    marketOpeningDateFormatter
-  ).format(new Date(marketOpeningDate * 1000));
-
-  const formattedOpeningHourAndMinute = new Intl.DateTimeFormat(
-    'hu-HU',
-    marketMinuteFormatter
-  ).format(new Date(marketOpeningDate * 1000));
-
-  const formattedClosingHourAndMinute = new Intl.DateTimeFormat(
-    'hu-HU',
-    marketMinuteFormatter
-  ).format(new Date(marketClosingDate * 1000));
-
-  const formattedOpeningAndClosingHour =
-    formattedOpeningHourAndMinute + ' - ' + formattedClosingHourAndMinute;
-
   return (
-    <div className="marketCard">
+    <div className='marketCard'>
       <MarketInfo
         profilePic={profilePic}
         marketName={marketName}
         marketLocation={marketLocation}
-        formattedYearMonthAndDay={formattedYearMonthAndDay}
-        formattedOpeningAndClosingHour={formattedOpeningAndClosingHour}
+        marketOpeningDate={marketOpeningDate}
+        marketClosingDate={marketClosingDate}
       />
-      <div className="vendorNumber">{vendorsAmount} árus</div>
+      <div className='vendorNumber'>{vendorsAmount} árus</div>
     </div>
   );
 };
--- frontend/src/components/MarketInfo.js
+++ frontend/src/components/MarketInfo.js
@@ -2,12 +2,41 @@ const MarketInfo = ({
   profilePic,
   marketName,
   marketLocation,
-  formattedYearMonthAndDay,
-  formattedOpeningAndClosingHour
+  marketOpeningDate,
+  marketClosingDate
 }) => {
+  const marketOpeningDateFormatter = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  const marketMinuteFormatter = {
+    hour: '2-digit',
+    minute: 'numeric'
+  };
+
+  const formattedYearMonthAndDay = new Intl.DateTimeFormat(
+    'hu-HU',
+    marketOpeningDateFormatter
+  ).format(new Date(marketOpeningDate * 1000));
+
+  const formattedOpeningHourAndMinute = new Intl.DateTimeFormat(
+    'hu-HU',
+    marketMinuteFormatter
+  ).format(new Date(marketOpeningDate * 1000));
+
+  const formattedClosingHourAndMinute = new Intl.DateTimeFormat(
+    'hu-HU',
+    marketMinuteFormatter
+  ).format(new Date(marketClosingDate * 1000));
+
+  const formattedOpeningAndClosingHour =
+    formattedOpeningHourAndMinute + ' - ' + formattedClosingHourAndMinute;
+
   return (
     <>
-      <img className='marketLogo' src={profilePic} alt='logo' />
+      <img className='marketLogo headerLogo' src={profilePic} alt='logo' />
       <div className='topMarketInfo'>
         <div className='marketName'>{marketName}</div>
         <div className='marketLocationAndDate'>
