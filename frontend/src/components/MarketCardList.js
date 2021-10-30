e106624180762e6e868af122eed99d2e9512b715
Szabó Gábor <91szabogabor@gmail.com>
2021-10-30 14:24:27 +0200

    add condition to exclude outdated markets from the list of markets

--- frontend/src/components/MarketCardList.js
+++ frontend/src/components/MarketCardList.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 const MarketCardList = () => {
   const [markets, setMarkets] = useState([]);
+  let currentTime = new Date();
 
   const getMarkets = async () => {
     const result = await fetchMarkets();
@@ -17,26 +18,30 @@ const MarketCardList = () => {
   }, []);
 
   return (
-    <div className='card-list'>
+    <div className="card-list">
       {markets.map((market) => {
-        return (
-          <div key={market.id}>
-            <Link
-              to={`/piacok/${market.id}`}
-              style={{ textDecoration: 'none' }}
-            >
-              <MarketCard
+        if (currentTime.getTime() / 1000 < market.closingDate) {
+          return (
+            <div key={market.id}>
+              <Link
+                to={`/piacok/${market.id}`}
                 style={{ textDecoration: 'none' }}
-                profilePic={market.profilePic}
-                marketName={market.name}
-                marketLocation={market.place}
-                marketOpeningDate={market.openingDate}
-                marketClosingDate={market.closingDate}
-                vendorsAmount={market.numberOfVendors}
-              />
-            </Link>
-          </div>
-        );
+              >
+                <MarketCard
+                  style={{ textDecoration: 'none' }}
+                  profilePic={market.profilePic}
+                  marketName={market.name}
+                  marketLocation={market.place}
+                  marketOpeningDate={market.openingDate}
+                  marketClosingDate={market.closingDate}
+                  vendorsAmount={market.numberOfVendors}
+                />
+              </Link>
+            </div>
+          );
+        } else {
+          return null;
+        }
       })}
     </div>
   );
