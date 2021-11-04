24f1e619b53bf5f1da3feead7de29d45c50f1747
Attila Csanyi <csaniattila23@gmail.com>
2021-11-04 16:03:04 +0100

    Create query

--- backend/src/main/java/org/flowacademy/hu/market/app/repositories/MarketRepository.java
+++ backend/src/main/java/org/flowacademy/hu/market/app/repositories/MarketRepository.java
@@ -14,4 +14,7 @@ public interface MarketRepository extends JpaRepository<Market, Long> {
 
     @Query("SELECT markets FROM Market markets WHERE markets.closingDate > UNIX_TIMESTAMP()")
     List<Market> findAllUpcomingMarkets();
+
+    @Query("SELECT markets FROM Market markets WHERE markets.closingDate > UNIX_TIMESTAMP()")
+    List<Market> findAllUpcomingMarketsByVendorId();
 }
