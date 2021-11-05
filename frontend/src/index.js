9299f11df2499c9adc9d6374420a846bc796c3b9
dzsak <sisakadam@gmail.com>
2021-11-05 12:35:48 +0100

    error boundary removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,7 @@ import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
