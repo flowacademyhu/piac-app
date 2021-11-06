be7edc8d9e852487722fb13558fa627e4b673348
dzsak <sisakadam@gmail.com>
2021-11-06 11:13:25 +0100

    sorting by name function added to vendor list of one market

--- frontend/src/components/VendorListOfOneMarket.js
+++ frontend/src/components/VendorListOfOneMarket.js
@@ -2,10 +2,16 @@ import { Link } from 'react-router-dom';
 import VendorCard from '../components/VendorCard';
 
 const VendorListOfOneMarket = ({ market }) => {
+  const sortedList = market.vendors.sort(function (a, b) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+  });
+
   return (
     <div className='card-list'>
       {market.vendors &&
-        market.vendors.map((vendor) => {
+        sortedList.map((vendor) => {
           return (
             <div key={vendor.id}>
               <Link
