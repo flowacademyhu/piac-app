561210759062ee7f3b160eb4488208cced34e965
dzsak <sisakadam@gmail.com>
2021-11-08 16:51:00 +0100

    name sorting function outsourced

--- frontend/src/components/VendorListOfOneMarket.js
+++ frontend/src/components/VendorListOfOneMarket.js
@@ -1,17 +1,14 @@
 import { Link } from 'react-router-dom';
 import VendorCard from '../components/VendorCard';
+import sortByName from './SortByName';
 
 const VendorListOfOneMarket = ({ market }) => {
-  const sortedList = market.vendors.sort(function (a, b) {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-    return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
-  });
+  market.vendors.sort(sortByName);
 
   return (
     <div className='card-list'>
       {market.vendors &&
-        sortedList.map((vendor) => {
+        market.vendors.map((vendor) => {
           return (
             <div key={vendor.id}>
               <Link
