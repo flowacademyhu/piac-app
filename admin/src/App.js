38e7c938e91f861fb7238ec55bd470c331d75c1c
Roland Németh <info@nemethroland.hu>
2021-11-08 14:39:14 +0100

    Format files with Prettier in admin folder

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Admin felület
-    </div>
-  );
+  return <div className="App">Admin felület</div>;
 }
 
 export default App;
