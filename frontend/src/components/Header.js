15901b8bbc48898554cf595e8a2dadb8067231e1
Erik <patoerik93@gmail.com>
2021-11-09 14:03:15 +0100

    Modified Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,40 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  NavLink,
+  useLocation,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
 import MarketLogo from "../icons/navigation/calendar-gray.svg";
 import VendorLogo from "../icons/navigation/vendor-dark-gray.svg";
+import "./Header.css";
 
 const Header = () => {
   const location = useLocation();
 
   return (
-    <div
-      className={
-        location.pathname === "/"
-          ? "header market-header"
-          : "header vendor-header"
-      }
-    >
-      {location.pathname === "/" ? (
-        <div>
-          <div className="MarketLogoContainer">
-            <img className="Logo" src={MarketLogo} alt="Logo" />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="Header">
+            <div className="MarketLogoContainer">
+              <img className="Logo" src={MarketLogo} alt="Logo" />
+            </div>
+            <div className="Markets">Piacok</div>
           </div>
-          <div className="Markets">Piacok</div>
-        </div>
-      ) : (
-        <div>
-          <div className="MarketLogoContainer">
-            <img className="Logo" src={VendorLogo} alt="Logo" />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path="/arusok">
+          <div className="Header">
+            <div className="MarketLogoContainer">
+              <img className="Logo" src={VendorLogo} alt="Logo" />
+            </div>
+            <div className="Vendors">Árusok</div>
           </div>
-          <div className="Vendors">Árusok</div>
-        </div>
-      )}
-    </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
-
 export default Header;
