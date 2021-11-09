1d590b050e06e89f48a0fcd5c0a962638970afad
Szabó Gábor <91szabogabor@gmail.com>
2021-11-09 11:07:17 +0100

    create pages folder for pages and placed new page into it

---
+++ admin/src/pages/TablesPage.js
@@ -0,0 +1,13 @@
+import MarketTable from "../components/MarketTable";
+import VendorTable from "../components/VendorTable";
+
+const TablesPage = () => {
+  return (
+    <>
+      <MarketTable />
+      <VendorTable />
+    </>
+  );
+};
+
+export default TablesPage;
