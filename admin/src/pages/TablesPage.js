be99e003f4c19157b599eee11acc5240cf739fcc
Szabó Gábor <91szabogabor@gmail.com>
2021-11-09 14:33:25 +0100

    add css class to TablesPage

--- admin/src/pages/TablesPage.js
+++ admin/src/pages/TablesPage.js
@@ -1,12 +1,13 @@
 import MarketTable from "../components/MarketTable";
 import VendorTable from "../components/VendorTable";
+import "../styles/Tables.css"
 
 const TablesPage = () => {
   return (
-    <>
+    <div className="tables">
       <MarketTable />
       <VendorTable />
-    </>
+    </div>
   );
 };
 
