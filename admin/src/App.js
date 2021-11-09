42ee6cab4b747b849449f52a56d88b4ca44819b3
Attila Csanyi <csaniattila23@gmail.com>
2021-11-09 22:03:27 +0100

    Create route for new vendor

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MarketTablePage from "./pages/MarketTablePage";
 import VendorTablePage from "./pages/VendorTablePage";
 import Menu from "./components/Menu";
+import VendorDetails from "./pages/VendorDetails";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/piac" element={<MarketTablePage />} />
           <Route exact path="/arus" element={<VendorTablePage />} />
+          <Route exact path="/arus/uj" element={<VendorDetails />} />
         </Routes>
       </Router>
     </>
