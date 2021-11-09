9d89abdaabc99f083b93a2e9c03c9adc1c128dc7
dzsak <sisakadam@gmail.com>
2021-11-09 20:47:51 +0100

    test skeleton added

---
+++ frontend/src/tests/filteredVendorArray.test.js
@@ -0,0 +1,6 @@
+const filteringArray = require("../components/filteringArray");
+const sum = require("../components/filteringArray");
+
+test("add 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
