b87c02e2986f763b93c11cd07b928e3e277e39fe
dzsak <sisakadam@gmail.com>
2021-11-10 19:13:54 +0100

    test cases added for filteredarraybykeyword function when some of the values is null

--- frontend/src/tests/filteredArrayByKeyword.test.js
+++ frontend/src/tests/filteredArrayByKeyword.test.js
@@ -4,7 +4,7 @@ const objectArray = [
   {
     id: 1,
     name: "Chilikirály",
-    intro: "Chilizz belünk, Magyarország legjobb chilijeivel...",
+    intro: null,
     profilePic: null,
     products: ["chilik", "paprikakrémek", "csípős szószok"],
     cardPayment: true,
@@ -32,6 +32,37 @@ const objectArray = [
   },
 ];
 
+const objectArrayWithValueNull = [
+  {
+    id: 1,
+    name: "Chilikirály",
+    intro: null,
+    profilePic: null,
+    products: ["chilik", "paprikakrémek", "csípős szószok"],
+    cardPayment: true,
+    email: "chiliking@flow.hu",
+    facebook: "Chiliking-Facebook",
+    instagram: null,
+    phone: "+36302345678",
+    webSite: null,
+    introductionLong: null,
+  },
+  {
+    id: 2,
+    name: "Just incase",
+    intro: null,
+    profilePic: null,
+    products: ["lebomló zacskók", "kézműves szappanok", "papírdobozok"],
+    cardPayment: true,
+    email: "csakugy@flow.hu",
+    facebook: "JustInCase-Facebook",
+    instagram: "justInCase.insta",
+    phone: "+36308765432",
+    webSite: "www.justincase.com",
+    introductionLong: null,
+  },
+];
+
 test("given an array with two object, when the search term is an empty string, then returns the full array", () => {
   expect(filteredArrayByKeyword(objectArray, "")).toStrictEqual(objectArray);
 });
@@ -52,12 +83,24 @@ test("given an array with two objects, when the search term is 'tacocat', then r
   expect(filteredArrayByKeyword(objectArray, "tacocat")).toStrictEqual([]);
 });
 
-test("given an array with two objects, when the search term is 'krem', then returns the first object of the array", () => {
-  expect(filteredArrayByKeyword(objectArray, "krem")).toStrictEqual([
-    objectArray[0],
-  ]);
-});
-
 test("given an array with two objects, when the search term is 'le', then returns the fully array", () => {
   expect(filteredArrayByKeyword(objectArray, "le")).toStrictEqual(objectArray);
+});
+
+test("given an array with two objects with some null values, when the search term is 'zo', then returns the fully array", () => {
+  expect(filteredArrayByKeyword(objectArrayWithValueNull, "zo")).toStrictEqual(
+    objectArrayWithValueNull
+  );
+});
+
+test("given an array with two object with some values null, when the search term is an empty string, then returns the full array", () => {
+  expect(filteredArrayByKeyword(objectArrayWithValueNull, "")).toStrictEqual(
+    objectArrayWithValueNull
+  );
+});
+
+test("given an array with two objects with some value null, when the search term is 'király', then returns the first object of the array", () => {
+  expect(
+    filteredArrayByKeyword(objectArrayWithValueNull, "papir dobozok")
+  ).toStrictEqual([objectArrayWithValueNull[1]]);
 });
