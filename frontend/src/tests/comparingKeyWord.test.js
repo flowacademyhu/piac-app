d32353642a9c2ade954d9de9fff2199a128851ec
dzsak <sisakadam@gmail.com>
2021-11-10 12:09:00 +0100

    plus tests added for comparing keyword test file

--- frontend/src/tests/comparingKeyWord.test.js
+++ frontend/src/tests/comparingKeyWord.test.js
@@ -1,17 +1,41 @@
 const textIsIncludesKeyword = require("../functions/textIsIncludesKeyword");
 
-test("given a string 'dog', when the search term is an empty string, then returns true", () => {
-  expect(textIsIncludesKeyword("dog", "")).toBe(true);
+test("given a long text, when the search term is an empty string, then returns true", () => {
+  expect(
+    textIsIncludesKeyword(
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+      ""
+    )
+  ).toBe(true);
 });
 
 test("given a string 'pirate', when the search term is 'PiRaTe', then returns true", () => {
-  expect(textIsIncludesKeyword("pirate", "PiRaTe")).toBe(true);
+  expect(textIsIncludesKeyword("pirate", "PIRate")).toBe(true);
 });
 
-test("given a string 'pirate', when the search term is 'cat', then returns false", () => {
-  expect(textIsIncludesKeyword("pirate", "cat")).toBe(false);
+test("given a long text, when the search term is 'cat', then returns false", () => {
+  expect(
+    textIsIncludesKeyword(
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+      "cat"
+    )
+  ).toBe(false);
 });
 
-test("given a string 'kitchen', when the search term is 'market', then returns false", () => {
-  expect(textIsIncludesKeyword("kitchen", "market")).toBe(false);
+test("given a long text, when the search term is 'lor', then returns true", () => {
+  expect(
+    textIsIncludesKeyword(
+      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+      "market"
+    )
+  ).toBe(false);
+});
+
+test("given a long text, when the search term is 'rocket', then returns false", () => {
+  expect(
+    textIsIncludesKeyword(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae.",
+      "rocket"
+    )
+  ).toBe(false);
 });
