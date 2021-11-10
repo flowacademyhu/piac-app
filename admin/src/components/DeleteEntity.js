f9148cd179340bf27cc3fdaecc9a3333fe4c823d
Szabó Gábor <91szabogabor@gmail.com>
2021-11-10 20:11:26 +0100

    change 'question' prop name to 'confirmationQuestion' in DeleteEntity.js

--- admin/src/components/DeleteEntity.js
+++ admin/src/components/DeleteEntity.js
@@ -2,7 +2,7 @@ import { Button, Modal } from "react-bootstrap";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const DeleteEntity = ({ question, handleDelete, ID }) => {
+const DeleteEntity = ({ confirmationQuestion, handleDelete, ID }) => {
   const [appear, setAppear] = useState(false);
   const handleClose = () => setAppear(false);
   const handleAppear = () => setAppear(true);
@@ -13,7 +13,7 @@ const DeleteEntity = ({ question, handleDelete, ID }) => {
         Töröl
       </Button>
       <Modal show={appear} onHide={handleClose}>
-        <Modal.Body>{question}</Modal.Body>
+        <Modal.Body>{confirmationQuestion}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Mégse
