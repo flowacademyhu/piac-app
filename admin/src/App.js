615f219ed82feb1da87983f9f5f30f56f4bddaf0
Attila Csanyi <csaniattila23@gmail.com>
2021-11-10 20:15:01 +0100

    Modify edit vendor path

--- admin/src/App.js
+++ admin/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route exact path="/piac" element={<MarketTablePage />} />
           <Route exact path="/arus" element={<VendorTablePage />} />
           <Route exact path="/arus/uj" element={<VendorDetails />} />
-          <Route exact path="/arus/szerkeszt" element={<VendorDetails />} />
+          <Route exact path="/arus/szerkeszt/:id" element={<VendorDetails />} />
         </Routes>
       </Router>
     </>
