a88a5d914c644b94c456d0f3171d46ca87464c6e
Erik <patoerik93@gmail.com>
2021-11-12 09:47:50 +0100

    Refactored login js

--- admin/src/components/Login.js
+++ admin/src/components/Login.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 import "./login.css";
 
