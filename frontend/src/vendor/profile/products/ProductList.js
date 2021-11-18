5f84f0b641817da69118b441ee09f07da67d5889
Roland Németh <info@nemethroland.hu>
2021-11-18 14:47:16 +0100

    Move vendor profile components to vendor profile folder

--- frontend/src/vendor/profile/products/ProductList.js
+++ frontend/src/vendor/profile/products/ProductList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import tag_Icon from "../icons/tag.svg";
+import tag_Icon from "./tag.svg";
 
 const ProductList = ({ products }) => {
   const productList = products
--- frontend/src/vendor/profile/products/Products.js
+++ frontend/src/vendor/profile/products/Products.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ProductList from "./ProductList";
-import "../styles/Products.css";
+import "../../../styles/Products.css";
 
 const Products = ({ products }) => {
   return (
