81597d6a93eb8042b9b83dddb9f8f113197c0cd1
Roland Németh <info@nemethroland.hu>
2021-11-18 15:17:37 +0100

    Move VendorHeader CSS closer to the component

--- frontend/src/vendor/profile/VendorHeader.js
+++ frontend/src/vendor/profile/VendorHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/vendor-header.css";
+import "./VendorHeader.css";
 import defaultVendorImage from "../defaultVendorImage.png";
 
 const VendorHeader = (props) => {
