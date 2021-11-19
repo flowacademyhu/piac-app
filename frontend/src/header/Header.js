785735f3add31eda3c76a3a3ed11f01c3cee5992
Szabó Gábor <91szabogabor@gmail.com>
2021-11-19 15:38:58 +0100

    add HeaderContainer, Logo, and LogoContainer as styled-components

--- frontend/src/header/Header.js
+++ frontend/src/header/Header.js
@@ -1,29 +1,63 @@
 import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 import MarketLogo from "../icons/navigation/calendar-dark-gray.svg";
 import VendorLogo from "../icons/navigation/vendor-dark-gray.svg";
-import "./Header.css";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-position: center;
+  position: relative;
+  top: 0;
+  font-family: "Amatic SC", sans-serif;
+  font-weight: 600;
+  font-size: 40px;
+  color: #33221a;
+  z-index: 1;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const Logo = styled.img`
+  display: flex;
+  width: 40px;
+  height: 40px;
+`;
+
+const LogoContainer = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: grid;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #ffffff;
+`;
 
 const Header = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/">
-          <div className="Header">
-            <div className="LogoContainer">
-              <img className="Logo" src={MarketLogo} alt="Logo" />
-            </div>
+          <HeaderContainer>
+            <LogoContainer>
+              <Logo src={MarketLogo} alt="Logo" />
+            </LogoContainer>
             <div className="Markets">Piacok</div>
-          </div>
+          </HeaderContainer>
         </Route>
       </Switch>
       <Switch>
         <Route exact path="/arusok">
-          <div className="Header">
-            <div className="LogoContainer">
-              <img className="Logo" src={VendorLogo} alt="Logo" />
-            </div>
+          <HeaderContainer>
+            <LogoContainer>
+              <Logo src={VendorLogo} alt="Logo" />
+            </LogoContainer>
             <div className="Vendors">Árusok</div>
-          </div>
+          </HeaderContainer>
         </Route>
       </Switch>
     </div>
