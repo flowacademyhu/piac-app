f20c32fe2fb6d388440dddd197d0ba802262afb2
Szabó Gábor <91szabogabor@gmail.com>
2021-11-19 16:05:19 +0100

    add CardList as styled-components

--- frontend/src/market/list/MarketCardList.js
+++ frontend/src/market/list/MarketCardList.js
@@ -1,9 +1,16 @@
 import MarketCard from "../MarketCard";
 import React, { useState, useLayoutEffect } from "react";
-import "../../styles/CardList.css";
 import { fetchUpcomingMarkets } from "../../api/Service";
 import { Link } from "react-router-dom";
 import ErrorBody from "../../components/ErrorBody";
+import styled from "styled-components";
+
+const CardList = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  padding: 20px 20px 110px;
+`;
 
 const MarketCardList = () => {
   const [upcomingMarkets, setUpcomingMarkets] = useState([]);
@@ -19,7 +26,7 @@ const MarketCardList = () => {
   }, []);
 
   return (
-    <div className="card-list">
+    <CardList>
       {error ? (
         <ErrorBody error={error} />
       ) : (
@@ -44,7 +51,7 @@ const MarketCardList = () => {
           );
         })
       )}
-    </div>
+    </CardList>
   );
 };
 
