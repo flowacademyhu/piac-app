4f2dde7b3dfe05821fda6e7022c303189035d8ec
Szabó Gábor <91szabogabor@gmail.com>
2021-11-20 16:05:24 +0100

    add TopInfoText and BottomInfoText as styled-components

--- frontend/src/market/profile/VendorlistUploadInProgress.js
+++ frontend/src/market/profile/VendorlistUploadInProgress.js
@@ -1,14 +1,28 @@
 import "./VendorlistUploadInProgress.css";
+import styled from "styled-components";
+
+const TopInfoText = styled.div`
+  font-family: "Open Sans", sans-serif;
+  font-weight: bold;
+  font-size: 20px;
+  padding-bottom: 20px;
+`;
+
+const BottomInfoText = styled.div`
+  font-family: "Open Sans", sans-serif;
+  font-style: normal;
+  font-size: 12px;
+`;
 
 const VendorlistUploadInProgress = ({ title, body }) => {
   return (
-    <div className="card-list inProgressText">
-      <div className="uploadTexts topInfoText">
+    <div className="card-list">
+      <TopInfoText>
         <div>{title}</div>
-      </div>
-      <div className="uploadTexts bottomInfoText">
+      </TopInfoText>
+      <BottomInfoText>
         <div>{body}</div>
-      </div>
+      </BottomInfoText>
     </div>
   );
 };
