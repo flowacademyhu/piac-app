88e7e23cddb0c9cbecfec9e23b32bfd531f1345d
Szabó Gábor <91szabogabor@gmail.com>
2021-11-21 18:30:12 +0100

    add MarketHeader as styled-component

--- frontend/src/market/profile/HeaderWithMarket.js
+++ frontend/src/market/profile/HeaderWithMarket.js
@@ -1,5 +1,20 @@
 import MarketInfo from "./MarketInfo";
 import "./MarketCard.css";
+import styled from "styled-components";
+
+const MarketHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: max-content;
+  user-select: none;
+  padding: 20px 20px 0px 20px;
+  background-color: #f7f5f2;
+  width: 100%;
+  color: #33221a;
+`;
 
 const HeaderWithMarket = ({
   profilePic,
@@ -9,7 +24,7 @@ const HeaderWithMarket = ({
   marketClosingDate,
 }) => {
   return (
-    <div className="marketHeader">
+    <MarketHeader>
       <MarketInfo
         header
         marketHeaderLogoStyle
@@ -19,7 +34,7 @@ const HeaderWithMarket = ({
         marketOpeningDate={marketOpeningDate}
         marketClosingDate={marketClosingDate}
       />
-    </div>
+    </MarketHeader>
   );
 };
 
