d52cd59c8b0a8f5e539e20ec08717190ce16f15f
Szabó Gábor <91szabogabor@gmail.com>
2021-11-22 13:14:54 +0100

    add VendorCardContainer as styled-component

--- frontend/src/vendor/VendorCard.js
+++ frontend/src/vendor/VendorCard.js
@@ -1,9 +1,23 @@
-import "./VendorCard.css";
+// import "./VendorCard.css";
 import defaultVendorImage from "./defaultVendorImage.png";
+import styled from "styled-components";
+
+const VendorCardContainer = styled.div`
+  display: flex;
+  flex-direction: row wrap;
+  cursor: pointer;
+  user-select: none;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;
+  padding: 10px;
+  background-color: #fffdfc;
+  width: 100%;
+  position: relative;
+`;
 
 const VendorCard = ({ vendor, vendorDesc, imageLogo }) => {
   return (
-    <div className="vendorCard">
+    <VendorCardContainer>
       <div
         className="vendorLogo"
         style={
@@ -20,7 +34,7 @@ const VendorCard = ({ vendor, vendorDesc, imageLogo }) => {
         <div className="vendorName">{vendor}</div>
         <div className="vendorDescription">{vendorDesc}</div>
       </div>
-    </div>
+    </VendorCardContainer>
   );
 };
 
