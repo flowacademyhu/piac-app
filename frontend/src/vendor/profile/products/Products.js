78528d7631ebf849b918b4a16f0692985cd2c329
Szabó Gábor <91szabogabor@gmail.com>
2021-11-22 11:16:44 +0100

    add Product styled-component

--- frontend/src/vendor/profile/products/Products.js
+++ frontend/src/vendor/profile/products/Products.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ProductList from "./ProductList";
 import "./Products.css";
+import styled from "styled-components";
+
+const Product = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 3;
+  padding: 0 20px;
+  margin-bottom: 20px;
+`;
 
 const Products = ({ products }) => {
   return (
-    <div className="product vendor-padding">
+    <Product>
       <div className="product-content">
         <div className="product-title">Mit tudsz venni nálam?</div>
         <ProductList products={products} />
       </div>
-    </div>
+    </Product>
   );
 };
 
