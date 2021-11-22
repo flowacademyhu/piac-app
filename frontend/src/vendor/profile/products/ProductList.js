fc392446ff28a0eb5b8687bb4f0f7ba87299a7ba
Szabó Gábor <91szabogabor@gmail.com>
2021-11-22 11:09:28 +0100

    add ProductItem styled-component

--- frontend/src/vendor/profile/products/ProductList.js
+++ frontend/src/vendor/profile/products/ProductList.js
@@ -1,13 +1,21 @@
 import React from "react";
 import tag_Icon from "./tag.svg";
+import styled from "styled-components";
+
+const ProductItem = styled.div`
+  text-align: center;
+  font-family: "Bebas Neue", sans-serif;
+  font-size: 24px;
+  padding-right: 20px;
+`;
 
 const ProductList = ({ products }) => {
   const productList = products
     ? products.map((product, index) => (
-        <div key={index} className="product-item">
+        <ProductItem key={index}>
           <img className="product-tag" src={tag_Icon} alt="tag" />
           {product}
-        </div>
+        </ProductItem>
       ))
     : [];
   return <div className="product-list">{productList}</div>;
