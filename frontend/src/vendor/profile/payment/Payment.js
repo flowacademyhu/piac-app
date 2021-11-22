dde284f9483332cf5716f4e06e25f90cd619e546
Szabó Gábor <91szabogabor@gmail.com>
2021-11-22 10:59:31 +0100

    add VendorPayment styled-component

--- frontend/src/vendor/profile/payment/Payment.js
+++ frontend/src/vendor/profile/payment/Payment.js
@@ -1,20 +1,30 @@
 import "./payment.css";
 import cardIcon from "./card.svg";
 import cashIcon from "./cash.svg";
+import styled from "styled-components";
+
+const VendorPayment = styled.div`
+  display: flex;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 400;
+  font-size: 10px;
+  padding: 0 20px;
+  margin-bottom: 20px;
+`;
 
 const Payment = ({ cardPayment }) => {
   const paymentText = (cardPayment) =>
     cardPayment ? "Bankkártyával is fizethetsz" : "Csak készpénzzel fizethetsz";
 
   return (
-    <div className="vendor-payment vendor-padding">
+    <VendorPayment>
       <img
         className="payment-icon"
         src={cardPayment ? cardIcon : cashIcon}
         alt=""
       />
       <span className="payment-text">{paymentText(cardPayment)}</span>
-    </div>
+    </VendorPayment>
   );
 };
 
