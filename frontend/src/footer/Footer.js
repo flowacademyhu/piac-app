1ab8a2c746b6bc3e24e7f39e1ddacef67fcb53aa
Erik <patoerik93@gmail.com>
2021-11-25 13:27:51 +0100

    Deleted container and row

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -1,20 +1,11 @@
 import "./Footer.css";
-import { Container, Row } from "react-bootstrap";
 import FooterButton from "./FooterButton";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <Container className="container-fluid">
-        <Row className="footer-row">
-          <FooterButton requestParam="/" appelation="PIACOK" logo="market" />
-          <FooterButton
-            requestParam="/arusok"
-            appelation="ÁRUSOK"
-            logo="vendor"
-          />
-        </Row>
-      </Container>
+      <FooterButton requestParam="/" appelation="PIACOK" logo="market" />
+      <FooterButton requestParam="/arusok" appelation="ÁRUSOK" logo="vendor" />
     </div>
   );
 };
