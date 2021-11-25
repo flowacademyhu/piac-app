24532f31cb7aeaf3213bd5655413be35f1742605
Erik <patoerik93@gmail.com>
2021-11-25 14:14:02 +0100

    Edited css into component

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -1,12 +1,30 @@
+import styled from "styled-components";
 import "./Footer.css";
 import FooterButton from "./FooterButton";
 
 const Footer = () => {
+  const FooterContainer = styled.div`
+    padding-top: 5px;
+    text-decoration: none;
+    background-color: #fffdfc;
+    position: fixed;
+    vertical-align: middle;
+    bottom: 0;
+    width: 100%;
+    border-top: solid;
+    border-color: #53b896;
+    border-width: 0.5px;
+    border-top-width: 2px;
+    height: 60px;
+    display: flex;
+    justify-content: space-around;
+  `;
+
   return (
-    <div className="footer">
+    <FooterContainer>
       <FooterButton requestParam="/" appelation="PIACOK" logo="market" />
       <FooterButton requestParam="/arusok" appelation="ÁRUSOK" logo="vendor" />
-    </div>
+    </FooterContainer>
   );
 };
 
