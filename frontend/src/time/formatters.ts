a0f099cfee04abc5959159f110ee0388df90a14e
Roland Németh <info@nemethroland.hu>
2021-11-25 01:45:09 +0100

    Extract out time formatting utilities

---
+++ frontend/src/time/formatters.ts
@@ -0,0 +1,32 @@
+export const dateFormat = new Intl.DateTimeFormat("hu-HU", {
+  month: "long",
+  day: "numeric",
+  timeZone: "Europe/Budapest",
+});
+
+export const timeFormat = new Intl.DateTimeFormat("hu-HU", {
+  hour: "2-digit",
+  minute: "numeric",
+  timeZone: "Europe/Budapest",
+});
+
+export const monthFormat = new Intl.DateTimeFormat("hu-HU", {
+  month: "short",
+  timeZone: "Europe/Budapest",
+});
+
+export const dayDigitsFormat = new Intl.DateTimeFormat("hu-HU", {
+  day: "2-digit",
+  timeZone: "Europe/Budapest",
+});
+
+export const weekdayFormat = new Intl.DateTimeFormat("hu-HU", {
+  weekday: "long",
+  timeZone: "Europe/Budapest",
+});
+
+export const minuteFormat = new Intl.DateTimeFormat("hu-HU", {
+  hour: "2-digit",
+  minute: "numeric",
+  timeZone: "Europe/Budapest",
+});
