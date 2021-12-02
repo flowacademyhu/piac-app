04add4c63f2a6f534eeda0e86bf6a3f1f0de4110
Erik <patoerik93@gmail.com>
2021-12-02 17:18:00 +0100

    Styled component outside of render

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -1,24 +1,24 @@
 import styled from "styled-components";
 import FooterButton from "./FooterButton";
 
-const Footer = () => {
-  const FooterContainer = styled.div`
-    text-decoration: none;
-    background-color: #fffdfc;
-    position: fixed;
-    vertical-align: middle;
-    bottom: 0;
-    width: 100%;
-    border-top: solid;
-    border-color: #53b896;
-    border-width: 0.5px;
-    border-top-width: 2px;
-    height: 60px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-  `;
+const FooterContainer = styled.div`
+  text-decoration: none;
+  background-color: #fffdfc;
+  position: fixed;
+  vertical-align: middle;
+  bottom: 0;
+  width: 100%;
+  border-top: solid;
+  border-color: #53b896;
+  border-width: 0.5px;
+  border-top-width: 2px;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 
+const Footer = () => {
   return (
     <FooterContainer>
       <FooterButton requestParam="/" appelation="PIACOK" logo="market" />
