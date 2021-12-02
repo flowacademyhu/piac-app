1de937b028f22c42c4e6a668742459fdc29e8fae
Roland Németh <info@nemethroland.hu>
2021-12-02 12:25:20 +0100

    Move VendorList component to a separate file

--- frontend/src/market/profile/VendorsByMarketPage.tsx
+++ frontend/src/market/profile/VendorsByMarketPage.tsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import HeaderWithMarket from "./HeaderWithMarket";
-import VendorListOfOneMarket from "./VendorListOfOneMarket";
-import VendorlistUploadInProgress from "./VendorlistUploadInProgress";
 import { fetchMarketById } from "../../api/Service";
 import { Redirect } from "react-router";
-import SearchArea from "../../components/SearchArea";
-import filteredArrayByKeyword from "../../vendor/filter";
 import { useQuery } from "react-query";
-import EmailContact from "../../components/EmailContact";
 import styled from "styled-components";
 import IdPrameter from "../../types/IdParameter";
-import Market from "market/Market";
+import VendorList from "./VendorList";
 
 const Intro = styled.h3`
   font-family: "Amatic SC", sans-serif;
@@ -20,55 +14,6 @@ const Intro = styled.h3`
   padding: 10px 0px 0px 20px;
   margin-bottom: 0;
 `;
-
-const MarketLoading = styled.div`
-  background-color: #f7f5f2;
-  width: 100%;
-  position: relative;
-  height: 100px;
-`;
-
-interface VendorListProps {
-  market: Market;
-}
-
-const VendorList = ({ market }: VendorListProps) => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  if (market && market.vendors && market.vendors.length > 0) {
-    return (
-      <>
-        <SearchArea
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-          placeHolder="Keress termékre vagy árusra..."
-        />
-        <VendorListOfOneMarket
-          vendors={filteredArrayByKeyword(market.vendors, searchTerm)}
-        />
-        <EmailContact />
-      </>
-    );
-  } else if (market && market.id) {
-    return (
-      <>
-        <VendorlistUploadInProgress
-          title="Szervezés alatt..."
-          body="Itt fogod megtalálni az árusokat, akik ezen a piacon jelen lesznek."
-        />
-        <EmailContact />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <MarketLoading />
-        <VendorlistUploadInProgress />
-      </>
-    );
-  }
-};
 
 const VendorsByMarketPage = () => {
   const marketId = useParams<IdPrameter>().id;
