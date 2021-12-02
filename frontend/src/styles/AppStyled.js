5efc2866b176a3460c003a61fb390d99105a94e2
Erik <patoerik93@gmail.com>
2021-12-02 14:17:58 +0100

    Modified global headings

--- frontend/src/styles/AppStyled.js
+++ frontend/src/styles/AppStyled.js
@@ -4,7 +4,24 @@ const AppStyled = createGlobalStyle`
 body,
 html {
   font-family: "Open Sans", sans-serif;
+  box-sizing: border-box;
   background-color: #f7f5f2;
+  font-size: 1rem;
+  font-weight: 400;
+  -webkit-text-size-adjust: 100%;
+}
+
+*, ::after, ::before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+.h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6{
+    margin-top: 0;
+    margin-bottom: .5rem;
+    font-weight: 500;
+    line-height: 1.2;
 }
 `;
 
