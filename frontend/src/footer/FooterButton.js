ebd5548203e9b5aeef7a44a806eab842f771e8e3
Erik <patoerik93@gmail.com>
2021-12-02 17:11:40 +0100

    Styled divs outside of render

--- frontend/src/footer/FooterButton.js
+++ frontend/src/footer/FooterButton.js
@@ -6,36 +6,36 @@ import VendorLogo from "../icons/navigation/vendor-gray.svg";
 import VendorLogoHighlight from "../icons/navigation/vendor-primary.svg";
 import styled from "styled-components";
 
+const FooterLabel = styled.div`
+  font-family: "Bebas Neue", sans-serif;
+  font-size: 18px;
+  color: #ccbeb8;
+  text-decoration: none;
+  outline-style: none;
+  display: flex;
+  justify-content: center;
+`;
+
+const FooterLabelActive = styled(FooterLabel)`
+  color: #53b896;
+`;
+
+const FooterLogo = styled.div`
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: auto;
+  text-decoration: none;
+  text-align: center;
+`;
+const NavLink = styled(Link)`
+  text-decoration: none;
+  text-align: center;
+`;
+
 const FooterButton = ({ requestParam, appelation, logo }) => {
   const location = useLocation();
-
-  const FooterLabel = styled.div`
-    font-family: "Bebas Neue", sans-serif;
-    font-size: 18px;
-    color: #ccbeb8;
-    text-decoration: none;
-    outline-style: none;
-    display: flex;
-    justify-content: center;
-  `;
-
-  const FooterLabelActive = styled(FooterLabel)`
-    color: #53b896;
-  `;
-
-  const FooterLogo = styled.div`
-    height: 25px;
-    display: flex;
-    justify-content: center;
-    text-align: center;
-    margin: auto;
-    text-decoration: none;
-    text-align: center;
-  `;
-  const NavLink = styled(Link)`
-    text-decoration: none;
-    text-align: center;
-  `;
 
   const IsActive =
     location.pathname === requestParam ? FooterLabelActive : FooterLabel;
