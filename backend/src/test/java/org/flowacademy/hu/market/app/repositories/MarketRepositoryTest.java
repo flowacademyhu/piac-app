752ebe8ff077e1588f4d87b14f2c77534a5da86d
Roland Németh <info@nemethroland.hu>
2021-12-07 08:02:41 +0100

    Add @Transactional to MarketRepositoryTest

--- backend/src/test/java/org/flowacademy/hu/market/app/repositories/MarketRepositoryTest.java
+++ backend/src/test/java/org/flowacademy/hu/market/app/repositories/MarketRepositoryTest.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace=Replace.NONE)
+@Transactional
 class MarketRepositoryTest {
 
   @Autowired
