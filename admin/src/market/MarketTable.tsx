760afb2e735a66aaa30608ce66bc478ba017a087
Roland Németh <info@nemethroland.hu>
2021-12-09 14:40:39 +0100

    Test reviewer annotation @PatocskaiErik

--- admin/src/market/MarketTable.tsx
+++ admin/src/market/MarketTable.tsx
@@ -5,6 +5,8 @@ import DeleteEntity from "components/DeleteEntity";
 import { Link } from "react-router-dom";
 import { Market } from "./Market";
 
+// test
+
 const MarketTable = () => {
   const [allMarkets, setAllMarkets] = useState<Market[]>([]);
 
