cdc09e97018df8816bc66045458c2b4b58ab587a
Attila Csanyi <csaniattila23@gmail.com>
2021-12-13 20:16:07 +0100

    Use cypress type to modify test data #202 @PatocskaiErik

--- admin/cypress/integration/markets/edit.spec.ts
+++ admin/cypress/integration/markets/edit.spec.ts
@@ -9,23 +9,26 @@ describe("Edit market", () => {
   it("should fill form with data of market", () => {
     cy.contains("Szerkeszt").click();
     cy.get("h1").contains("Piac módosítása");
-    cy.get("input[data-test=name-input]").should("have.value", "Bödön Piac");
-    cy.get("input[data-test=profilePic-input]").should(
-      "have.value",
-      "https://example.com/bodon.png"
-    );
-    cy.get("textarea[data-test=place-input]").should(
-      "have.value",
-      "Szeged Pláza"
-    );
-    cy.get("input[data-test=openingDate-input]").should(
-      "have.value",
-      "2021-09-04T10:00"
-    );
-    cy.get("input[data-test=closingDate-input]").should(
-      "have.value",
-      "2021-09-04T17:00"
-    );
+    cy.get("input[data-test=name-input]")
+      .should("have.value", "Bödön Piac")
+      .clear()
+      .type("Popup Piac");
+    cy.get("input[data-test=profilePic-input]")
+      .should("have.value", "https://example.com/bodon.png")
+      .clear()
+      .type("https://example.com/popup.png");
+    cy.get("textarea[data-test=place-input]")
+      .should("have.value", "Szeged Pláza")
+      .clear()
+      .type("Szálka Halászcsárda és Rendezvényház");
+    cy.get("input[data-test=openingDate-input]")
+      .should("have.value", "2021-09-04T10:00")
+      .clear()
+      .type("2021-12-18T09:30");
+    cy.get("input[data-test=closingDate-input]")
+      .should("have.value", "2021-09-04T17:00")
+      .clear()
+      .type("2021-12-18T17:30");
     cy.get("button").contains("Módosítás");
     cy.get("button").contains("Mégsem");
   });
