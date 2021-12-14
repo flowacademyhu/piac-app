21697f4c94c2ddc0706611fc7ba5e305730b1a36
Attila Csanyi <csaniattila23@gmail.com>
2021-12-14 10:48:03 +0100

    Create test to check request body #202 @PatocskaiErik

--- admin/cypress/integration/markets/edit.spec.ts
+++ admin/cypress/integration/markets/edit.spec.ts
@@ -1,12 +1,43 @@
+import { Market } from "market/Market";
+
 describe("Edit market", () => {
+  let newMarket: Market = {
+    numberOfVendors: 0,
+    id: "",
+    name: "",
+    profilePic: "",
+    place: "",
+    openingDate: 0,
+    closingDate: 0,
+  };
+
   beforeEach(() => {
-    cy.intercept("/v1/api/market", { fixture: "markets.json" });
+    cy.fixture("markets.json").then((markets) => {
+      cy.intercept("GET", "/v1/api/market", (req) => {
+        req.reply(
+          newMarket.id ? [{ ...newMarket }, ...markets.slice(1)] : markets
+        );
+      });
+    });
+
     cy.intercept("/v1/api/market/1", { fixture: "market-1.json" });
+
     cy.login("john@example.com");
     cy.visit("./piac");
   });
 
   it("should modify the market", () => {
+    cy.intercept("PUT", "/v1/api/admin/market/1", (req) => {
+      expect(req.body.name).to.equal("Popup Piac");
+      expect(req.body.profilePic).to.equal("https://example.com/popup.png");
+      expect(req.body.place).to.equal("Szálka Halászcsárda és Rendezvényház");
+      expect(req.body.openingDate).to.equal(1639816200);
+      expect(req.body.closingDate).to.equal(1639845000);
+      expect(req.headers.authorization).to.include("Bearer eyJhb");
+      newMarket = { id: 1, ...req.body };
+      req.reply(newMarket);
+    }).as("putMarketRequest");
+
     cy.contains("Szerkeszt").click();
     cy.get("h1").contains("Piac módosítása");
     cy.get("input[data-test=name-input]")
@@ -29,7 +60,12 @@ describe("Edit market", () => {
       .should("have.value", "2021-09-04T17:00")
       .clear()
       .type("2021-12-18T17:30");
-    cy.get("button").contains("Módosítás");
     cy.get("button").contains("Mégsem");
+    cy.get("button").contains("Módosítás").click();
+
+    cy.wait("@putMarketRequest").its("response.statusCode").should("eq", 200);
+
+    cy.contains("Popup Piac");
+    cy.contains("Szálka Halászcsárda és Rendezvényház");
   });
 });
