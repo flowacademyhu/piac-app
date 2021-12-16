a003a9b3ec73240b0170279f4f0bf0364814baa8
Szabó Gábor <91szabogabor@gmail.com>
2021-12-16 14:11:12 +0100

    delete redundant "List" part to conform to other tests @6uliver

--- admin/cypress/integration/markets/list.spec.ts
+++ admin/cypress/integration/markets/list.spec.ts
@@ -1,21 +1,19 @@
-describe("Markets", () => {
-  describe("List", () => {
-    beforeEach(() => {
-      cy.login("john@example.com");
-      cy.intercept("/v1/api/market", { fixture: "markets.json" });
+describe("Market list", () => {
+  beforeEach(() => {
+    cy.login("john@example.com");
+    cy.intercept("/v1/api/market", { fixture: "markets.json" });
 
-      cy.visit("/piac");
-    });
+    cy.visit("/piac");
+  });
 
-    it("should open market list", () => {
-      cy.contains("Piacok");
+  it("should open market list", () => {
+    cy.contains("Piacok");
 
-      cy.get("table tbody tr").as("rows");
+    cy.get("table tbody tr").as("rows");
 
-      cy.get("@rows").should("have.length", 2);
+    cy.get("@rows").should("have.length", 2);
 
-      cy.get("@rows").eq(0).contains("Bödön Piac");
-      cy.get("@rows").eq(1).contains("Böba Piac Karácsony");
-    });
+    cy.get("@rows").eq(0).contains("Bödön Piac");
+    cy.get("@rows").eq(1).contains("Böba Piac Karácsony");
   });
 });
