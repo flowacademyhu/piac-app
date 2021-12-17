49453248be7c23e952c429e04b0859c2ad5eb6b8
Erik <patoerik93@gmail.com>
2021-12-17 15:39:24 +0100

    Auto stash before rebase of "origin/develop"
    Added backend test cases for vendor

--- backend/src/test/java/org/flowacademy/hu/market/app/services/VendorServiceTest.java
+++ backend/src/test/java/org/flowacademy/hu/market/app/services/VendorServiceTest.java
@@ -1,37 +1,71 @@
 package org.flowacademy.hu.market.app.services;
 
 import org.flowacademy.hu.market.app.entities.Vendor;
+import org.flowacademy.hu.market.app.model.DetailVendorDTO;
+import org.flowacademy.hu.market.app.model.SimpleVendorDTO;
 import org.flowacademy.hu.market.app.repositories.VendorRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class VendorServiceTest {
 
-    VendorRepository vendorRepository = Mockito.mock(VendorRepository.class);
+    @Mock
+    VendorRepository vendorRepository;
+
+    @InjectMocks
+    VendorService vendorService;
 
     @Test
-    void deleteAllVendors() {
-        Vendor vendor = new Vendor();
-        Vendor vendor1 = new Vendor();
-        Vendor vendor2 = new Vendor();
-        vendorRepository.save(vendor);
-        vendorRepository.save(vendor1);
-        vendorRepository.save(vendor2);
-        vendorRepository.deleteAll();
-        Optional optional = vendorRepository.findById(vendor.getId());
-        assertEquals(Optional.empty(), optional);
+    public void should_delete_vendor_by_id_with_deleteById_method() {
+        vendorService.deleteVendorById(5261l);
+
+        Mockito.verify(vendorRepository).deleteById(5261l);
     }
 
     @Test
-    void deleteVendorById() {
-        Vendor vendor = new Vendor();
-        vendorRepository.save(vendor);
-        vendorRepository.deleteById(vendor.getId());
-        Optional optional = vendorRepository.findById(vendor.getId());
-        assertEquals(Optional.empty(), optional);
+    public void should_add_a_vendor_and_check_their_informations_are_correct() {
+        Vendor vendor = new Vendor()
+                .setIntro("Rövid bemutatkozás")
+                .setName("Obi-wan Kenobi")
+                .setProfilePic("profilpic")
+                .setId(123456l)
+                .setProducts(new HashSet<>(Arrays.asList("Alma", "Körte", "Barack")))
+                .setIntroductionLong("Hosszú bemutatkozás");
+
+        SimpleVendorDTO simpleDTO = vendorService.vendorToSimpleDTO(vendor);
+
+        Set<String> products = simpleDTO.getProducts();
+
+        assertEquals("Rövid bemutatkozás", simpleDTO.getIntro());
+        assertEquals("Obi-wan Kenobi", simpleDTO.getName());
+        assertEquals("profilpic", simpleDTO.getProfilePic());
+        assertEquals(123456l , simpleDTO.getId());
+        assertEquals(3, simpleDTO.getProducts().size());
+        assertEquals(products, simpleDTO.getProducts());
+        assertEquals("Hosszú bemutatkozás", simpleDTO.getIntroductionLong());
+    }
+
+    @Test
+    public void should_add_a_vendor_to_a_list_and_test_the_list_for_cases() {
+        List<Vendor> vendorList = new ArrayList<>();
+        vendorList.add(new Vendor().setName("Sajt"));
+        vendorList.add(new Vendor().setName("Alma"));
+        when(vendorRepository.findAll()).thenReturn(vendorList);
+
+        List<DetailVendorDTO> sortedList = vendorService.allVendors();
+
+        assertEquals(2, sortedList.size());
+        assertEquals("Alma", sortedList.get(0).getName());
+        assertEquals("Sajt", sortedList.get(1).getName());
     }
 }
