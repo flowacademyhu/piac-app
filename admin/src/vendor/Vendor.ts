c7c68742a989050f0b06e31122a6397f49645871
Szabó Gábor <91szabogabor@gmail.com>
2021-12-17 14:24:28 +0100

    add VendorInput and VendorWithId for useForm @6uliver

--- admin/src/vendor/Vendor.ts
+++ admin/src/vendor/Vendor.ts
@@ -1,4 +1,18 @@
-export default interface Vendor {
+export interface VendorInput {
   id: string;
   name: string;
+  profilePic: string;
+  intro: string;
+  introductionLong: string;
+  cardPayment: boolean;
+  products: Array<string>;
+  phone: string;
+  email: string;
+  facebook: string;
+  instagram: string;
+  webSite: string;
+}
+
+export interface VendorWithId extends VendorInput {
+  id: string;
 }
