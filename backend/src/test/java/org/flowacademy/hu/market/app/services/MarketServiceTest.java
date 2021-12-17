8f6553604823f748e0ac0f405e652f804867c669
Erik <patoerik93@gmail.com>
2021-12-17 15:45:46 +0100

    Fixed marketServiceTest

--- backend/src/test/java/org/flowacademy/hu/market/app/services/MarketServiceTest.java
+++ backend/src/test/java/org/flowacademy/hu/market/app/services/MarketServiceTest.java
@@ -1,37 +1,21 @@
 package org.flowacademy.hu.market.app.services;
 
-import org.flowacademy.hu.market.app.entities.Market;
 import org.flowacademy.hu.market.app.repositories.MarketRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MarketServiceTest {
 
-    private final MarketRepository marketRepository = Mockito.mock(MarketRepository.class);
+    @Mock
+    MarketRepository marketRepository;
+
+    @InjectMocks
+    MarketService marketService;
 
     @Test
-    void deleteAllMarkets() {
-        Market market = new Market();
-        Market market1 = new Market();
-        Market market2 = new Market();
-        marketRepository.save(market);
-        marketRepository.save(market1);
-        marketRepository.save(market2);
-        marketRepository.deleteAll();
-        Optional optional = marketRepository.findById(market.getId());
-        assertEquals(Optional.empty(), optional);
-    }
+    public void deleteMarketById() {
 
-    @Test
-    void deleteMarketById() {
-        Market market = new Market();
-        marketRepository.save(market);
-        marketRepository.deleteById(market.getId());
-        Optional optional = marketRepository.findById(market.getId());
-        assertEquals(Optional.empty(), optional);
     }
 }
