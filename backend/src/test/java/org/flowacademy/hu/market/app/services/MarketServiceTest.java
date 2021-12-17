c09579ae3aaf29d612d2cf84fde7460dbdf5f8ea
Erik <patoerik93@gmail.com>
2021-12-17 16:38:45 +0100

    Added disabled annotation to the MarketServiceTest

--- backend/src/test/java/org/flowacademy/hu/market/app/services/MarketServiceTest.java
+++ backend/src/test/java/org/flowacademy/hu/market/app/services/MarketServiceTest.java
@@ -1,11 +1,12 @@
 package org.flowacademy.hu.market.app.services;
 
 import org.flowacademy.hu.market.app.repositories.MarketRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-
+@Disabled
 class MarketServiceTest {
 
     @Mock
