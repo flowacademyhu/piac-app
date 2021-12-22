f3e2ac9aa82b3a9887fc335fd10775a15900faff
Szabó Gábor <91szabogabor@gmail.com>
2021-12-22 17:06:58 +0100

    add unit tests for adding vendor as admin and not admin @PatocskaiErik

--- backend/src/test/java/org/flowacademy/hu/market/app/controller/AdminVendorControllerTest.java
+++ backend/src/test/java/org/flowacademy/hu/market/app/controller/AdminVendorControllerTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -109,6 +110,72 @@ class AdminVendorControllerTest {
             .andExpect(status().isOk());
 
     verify(vendorService).updateVendor(eq(1337L), vendorDTOCaptor.capture());
+
+    VendorDTO actualVendorDTO = vendorDTOCaptor.getValue();
+
+    assertEquals("Példa", actualVendorDTO.getName());
+    assertEquals("shorturl.at/bkKW4", actualVendorDTO.getProfilePic());
+    assertEquals("Bemutatkozok röviden.", actualVendorDTO.getIntro());
+    assertFalse(actualVendorDTO.getCardPayment());
+    assertEquals("Példa", actualVendorDTO.getName());
+    assertEquals(new HashSet<>(Arrays.asList(123L, 456L, 789L)), actualVendorDTO.getMarketIds());
+    assertEquals("Bemutatkozok hosszan, mert bőbeszédű vagyok.", actualVendorDTO.getIntroductionLong());
+    assertEquals(new HashSet<>(Arrays.asList("termék 1", "termék 2", "termék 3")), actualVendorDTO.getProducts());
+    assertEquals("peldaemail@gmail.com", actualVendorDTO.getEmail());
+    assertEquals("peldafacebook", actualVendorDTO.getFacebook());
+    assertEquals("peldainstagram", actualVendorDTO.getInstagram());
+    assertEquals("06701234567", actualVendorDTO.getPhone());
+    assertEquals("https://www.peldaoldal.hu", actualVendorDTO.getWebSite());
+  }
+
+  @Test
+  void addingVendorIsForbiddenWithUnknownUser() throws Exception {
+    VendorDTO vendorDTO = new VendorDTO()
+            .setName("Példa")
+            .setProfilePic("shorturl.at/bkKW4")
+            .setIntro("Bemutatkozok röviden.")
+            .setCardPayment(false)
+            .setMarketIds(new HashSet<>(Arrays.asList(123L, 456L, 789L)))
+            .setIntroductionLong("Bemutatkozok hosszan, mert bőbeszédű vagyok.");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    mockMvc.perform(post("/v1/api/admin/vendor")
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(vendorDTO))
+                    .accept("application/json"))
+            .andExpect(status().isUnauthorized());
+
+    verify(vendorService, never()).addVendor(any());
+  }
+
+  @Test
+  @WithMockUser(username = "admin@example.com", roles = { "USER", "ADMIN" })
+  void addVendorWithAdminUser() throws Exception {
+    VendorDTO vendorDTO = new VendorDTO()
+            .setName("Példa")
+            .setProfilePic("shorturl.at/bkKW4")
+            .setIntro("Bemutatkozok röviden.")
+            .setCardPayment(false)
+            .setMarketIds(new HashSet<>(Arrays.asList(123L, 456L, 789L)))
+            .setIntroductionLong("Bemutatkozok hosszan, mert bőbeszédű vagyok.")
+            .setProducts(new HashSet<>(Arrays.asList("termék 1", "termék 2", "termék 3")))
+            .setEmail("peldaemail@gmail.com")
+            .setFacebook("peldafacebook")
+            .setInstagram("peldainstagram")
+            .setPhone("06701234567")
+            .setWebSite("https://www.peldaoldal.hu");
+
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    mockMvc.perform(post("/v1/api/admin/vendor")
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(vendorDTO))
+                    .accept("application/json"))
+            .andExpect(status().isOk());
+
+    verify(vendorService).addVendor(vendorDTOCaptor.capture());
 
     VendorDTO actualVendorDTO = vendorDTOCaptor.getValue();
 
